d72278dc16eb55f478c5c2ae321bb97cb1214557
Stikender <nadik17@bk.ru>
2022-12-10 14:07:16 +0800

    Finish commit base difficulty

--- src/Course1/CourseWork/main.java
+++ src/Course1/CourseWork/main.java
@@ -24,7 +24,6 @@ public class main {
         System.out.println(employeeMetods.printEmployeeMaxSalary(employees));
         System.out.println(employeeMetods.printAverageSalaryMonth(employees));
         System.out.println(employeeMetods.employeeFullName(employees));
-
     }
 }
 
